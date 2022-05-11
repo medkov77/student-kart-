74e41a4148d18f4bfb6911a5d0f54c263cedccb7
medkov77 <medkov77@mail.rum>
2022-05-11 20:45:03 +0300

    validation

--- src/app/pages/createStudendPage.jsx
+++ src/app/pages/createStudendPage.jsx
@@ -2,96 +2,102 @@ import React, { useEffect, useState } from "react";
 import { validator } from "../utils/validator";
 import TextField from "../components/common/form/textField";
 const CreateStudentPage = () => {
-    const [data, setData] = useState({});
+  const [data, setData] = useState({
+    name: "",
+    lastname: "",
+    yearOfBirth: "",
+    portfolio: "",
+  });
 
-    const [errors, setErrors] = useState({});
+  const [errors, setErrors] = useState({});
 
-    const handleChange = target => {
-        setData(prevState => ({
-            ...prevState,
-            [target.name]: target.value,
-        }));
-    };
-    const validatorConfig = {
-        name: {
-            isRequired: {
-                message: "Имя обязательно для заполнения",
-            },
-        },
-        lastname: {
-            isRequired: {
-                message: "Фамилия обязательна для заполнения",
-            },
-        },
-        yearOfBirth: {
-            isRequired: {
-                message: "Год рождения обязателен для заполнения",
-            },
-        },
-        portfolio: {
-            isRequired: {
-                message: "Портфолио обязательно для заполнения",
-            },
-        },
-    };
-    useEffect(() => {
-        validate();
-    }, [data]);
-    const validate = () => {
-        const errors = validator(data, validatorConfig);
-        setErrors(errors);
-        return Object.keys(errors).length === 0;
-    };
-    const isValid = Object.keys(errors).length === 0;
+  const handleChange = (target) => {
+    setData((prevState) => ({
+      ...prevState,
+      [target.name]: target.value,
+    }));
+  };
+  const validatorConfig = {
+    name: {
+      isRequired: {
+        message: "Имя обязательно для заполнения",
+      },
+    },
+    lastname: {
+      isRequired: {
+        message: "Фамилия обязательна для заполнения",
+      },
+    },
+    yearOfBirth: {
+      isRequired: {
+        message: "Год рождения обязателен для заполнения",
+      },
+    },
+    portfolio: {
+      isRequired: {
+        message: "Портфолио обязательно для заполнения",
+      },
+    },
+  };
+  const validate = () => {
+    const errors = validator(data, validatorConfig);
+    setErrors(errors);
+    return Object.keys(errors).length === 0;
+  };
+  useEffect(() => {
+    validate();
+  }, [data]);
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        const isValid = validate();
-        if (!isValid) return;
-    };
-    return (
-        <div className="container mt-4">
-            <h1>Создать</h1>
-            <form onSubmit={handleSubmit}>
-                <TextField
-                    label="Имя"
-                    name="name"
-                    value={data.email}
-                    onChange={handleChange}
-                    error={errors.email}
-                />
-                <TextField
-                    label="Фамилия"
-                    name="lastname"
-                    value={data.lastname}
-                    onChange={handleChange}
-                    error={errors.lastname}
-                />
-                <TextField
-                    label="Год рождения"
-                    name="yearOfBirth"
-                    value={data.yearOfBirth}
-                    onChange={handleChange}
-                    error={errors.yearOfBirth}
-                />
-                <TextField
-                    label="Портфолио"
-                    name="portfolio"
-                    value={data.portfolio}
-                    onChange={handleChange}
-                    error={errors.portfolio}
-                />
+  const isValid = Object.keys(errors).length === 0;
 
-                <button
-                    className="btn btn-primary "
-                    type="submit"
-                    disabled={!isValid}
-                >
-                    Создать
-                </button>
-            </form>
-        </div>
-    );
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const isValid = validate();
+    if (!isValid) return;
+  };
+  return (
+    <div className="container mt-4">
+      <h1>Создать</h1>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          type="text"
+          label="Имя"
+          name="name"
+          value={data.name}
+          onChange={handleChange}
+          error={errors.name}
+        />
+        <TextField
+          type="text"
+          label="Фамилия"
+          name="lastname"
+          value={data.lastname}
+          onChange={handleChange}
+          error={errors.lastname}
+        />
+        <TextField
+          type="number"
+          label="Год рождения"
+          name="yearOfBirth"
+          value={data.yearOfBirth}
+          onChange={handleChange}
+          error={errors.yearOfBirth}
+        />
+        <TextField
+          type="text"
+          label="Портфолио"
+          name="portfolio"
+          value={data.portfolio}
+          onChange={handleChange}
+          error={errors.portfolio}
+        />
+
+        <button className="btn btn-primary " type="submit" disabled={!isValid}>
+          Создать
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default CreateStudentPage;
